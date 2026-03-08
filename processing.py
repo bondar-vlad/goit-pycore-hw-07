0e6981572dee40248ba215bfcf2a4668b2341f81
vbondar <bondar.vlad@outlook.com>
2026-03-08 21:41:02 +0200

    AddressBook support added for all commands

--- processing.py
+++ processing.py
@@ -1,41 +1,51 @@
 from error_processing import input_error
-from models import AddressBook
+from models import AddressBook, Record
 
 @input_error
-def add_contact(args, contacts):
-    name, phone = args
-    contacts[name] = phone
-    return "Contact added."
+def add_contact(args, book: AddressBook):
+    name, phone, *_ = args
+    record = book.find(name)
+    message = "Contact updated."
+    if record is None:
+        record = Record(name)
+        book.add_record(record)
+        message = "Contact added."
+    if phone:
+        record.add_phone(phone)
+    return message
 
 @input_error
-def change_contact(args, contacts: dict):
-    name, phone = args
-    if name in contacts:
-        contacts[name] = phone
-        return "Contact updated."
-    return "Contact not found."
+def change_contact(args, contacts: AddressBook):
+    name, old_phone, new_phone, *_ = args
+    record = contacts.find(name)
+    message = "Contact updated."
+    if record is None:
+        message = "Contact not found."
+    else:
+        record.edit_phone(old_phone, new_phone)
+    return message    
 
 @input_error
-def contact_phone(args, contacts: dict):
-    name = args[0]
-    return contacts.get(name, "Contact not found.")
+def contact_phone(args, contacts: AddressBook):
+    name, *_ = args
+    return "\n".join(contacts.find(name).phones)
 
 @input_error
-def all_contacts(args, contacts: dict):
+def all_contacts(args, contacts: AddressBook):
     return "\n".join(list(contacts.values()))
 
 @input_error
 def add_birthday(args, book: AddressBook):
-    name, birthday = args
-    contact = book.find(name)
-    contact.add_birthday(birthday)
+    name, birthday, *_ = args
+    record = book.find(name)
+    record.add_birthday(birthday)
     return "Birthday added"
 
 @input_error
 def show_birthday(args, book: AddressBook):
-    name = args[0]
-    contact = book.find(name)
-    return contact.birthday
+    name, *_ = args
+    record = book.find(name)
+    return record.birthday
 
 @input_error
 def birthdays(args, book: AddressBook):
